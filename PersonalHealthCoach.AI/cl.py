788add8775794d6bf53c674d3b29bb121c61e59c
Brassat Alexandru <alrx.brss@gmail.com>
2023-04-02 12:38:17 +0300

    FitnessPlanner client

--- PersonalHealthCoach.AI/cl.py
+++ PersonalHealthCoach.AI/cl.py
@@ -1,20 +1,16 @@
 import requests
 
-url = 'http://localhost:8000/DietPlanner'
+url = 'http://localhost:8000/FitnessPlanner'
 
 while True:
     workout_id = input("Enter workout ID: ")
-    data = {
-    "Name": "George",
-    "ID" : "38284",
-    "Age": 21,
-    "Height" : 182,
-    "Weight" : 63,
-    "Sex" : "M",
-    "Objective" : "Gain muscular mass", "Diseases":[]}
-    response = requests.post(url, json=data)
-    """
-    print("Response status code: {}\n".format(response.status_code))
-    workout=response.json()["workout"]"""
-    print(response.json() )
-    print("\n")
+    data = {"workout_id": workout_id}
+    try:
+        response = requests.post(url, json=data)
+
+        print("Response status code: {}\n".format(response.status_code))
+        workout=response.json()["workout"]
+        print(workout)
+        print("\n")
+    except: 
+        print("Workout with ID: {} is not in the database\n\n".format(workout_id))
