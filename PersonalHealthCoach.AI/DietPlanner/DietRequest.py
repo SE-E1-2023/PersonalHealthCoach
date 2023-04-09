0f70b80b783f16bc727c4f37c4eaccde4be208d3
HowDidThat <maxim.mateibr@gmail.com>
2023-04-09 20:51:45 +0300

    M

--- PersonalHealthCoach.AI/DietPlanner/DietRequest.py
+++ PersonalHealthCoach.AI/DietPlanner/DietRequest.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 import os
+import random
 abspath = os.path.dirname(__file__)
 
 def matchingDiet(diet):
@@ -9,7 +10,12 @@ def matchingDiet(diet):
         data = json.load(file)
     usefulData = []
     for d in data["diets"]:
-        if diet in d["tag"]:
+       
+        check = True
+        for element in diet:
+            if element not in d["tag"]:
+                check = False
+        if check:
             usefulData.append(d)
     return usefulData
 
@@ -18,8 +24,9 @@ def newDiet(dietType,id):
     for diet in dietData:
         if diet["id"] != id:
             return diet
-    return dietData[0]
-
+    if len(dietData) != 0 :
+        return dietData[0]
+    return {}
 def knownUser(input):
     with open (f"{abspath}/Databases/Users.json","r") as file:
         users = json.load(file)
@@ -30,8 +37,22 @@ def knownUser(input):
             break
     response = {}
     response["diet"]= newDiet(input["dietType"],users["users"][userIndex]["idDiet"])
+    if len(response["diet"]) == 0:
+        return {}
     users["users"][userIndex]["idDiet"] = response["diet"]["id"]
-    with open(f"{abspath}/Databases/Users.json","w") as file:
+    response["breakfast"] = getMeal(input,users["users"][userIndex]["idBreakfast"],"Breakfast.json")
+    response["drink"] = getMeal(input,users["users"][userIndex]["idDrink"],"Drink.json")
+    response["mainCourse"] = getMeal(input,users["users"][userIndex]["idMainCourse"],"MainCourse.json")
+    response["sideDish"] = getMeal(input,users["users"][userIndex]["idSideDish"],"SideDish.json")
+    response["soup"] = getMeal(input,users["users"][userIndex]["idSnack"],"Soup.json")
+    response["snack"] = getMeal(input,users["users"][userIndex]["idSoup"],"Snack.json")
+    users["users"][userIndex]["idBreakfast"] = response["breakfast"]["id"]
+    users["users"][userIndex]["idDrink"] = response["drink"]["id"]
+    users["users"][userIndex]["idMainCourse"] = response["mainCourse"]["id"]
+    users["users"][userIndex]["idSideDish"] = response["sideDish"]["id"]
+    users["users"][userIndex]["idSnack"] = response["soup"]["id"]
+    users["users"][userIndex]["idSoup"] = response["snack"]["id"]
+    with open (f"{abspath}/Databases/Users.json","w") as file:
         json.dump(users,file,indent=2)
     return response
 
@@ -57,7 +78,7 @@ def getDiet(input):
     with open (f"{abspath}/Databases/Users.json","r") as file:
         users = json.load(file)
     good = 0
-    returndRequest={}
+    returnedRequest={}
     for user in users["users"]:
         if input["idClient"] == user["idUser"]:
             returnedRequest = knownUser(input)
@@ -65,16 +86,67 @@ def getDiet(input):
             break
     if good == 0:
         returnedRequest = unknownUser(input)
-    
     return returnedRequest
 
+def getMeal(info,idMeal,path):
+    with open (f"{abspath}/Databases/{path}","r") as file:
+        d = json.load(file)
+    minHealthiness = 0
+    maxHealthiness = 100
+    if (info["goal"] == "weight loss"):
+        minHealthiness = 30
+    if (info["goal"] == "weight gain"):
+        maxHealthiness = 30
+    data = d["meals"]
+    goodData = []
+    good = True
+    for meal in data:
+        good = True
+        for problem in info["dietType"]:
+            if meal[problem] == False:
+                good = False
+        for alergi in info["alergies"]:
+            if alergi in meal["ingredients"] : 
+                good = False
+        if meal["healthScore"] < minHealthiness or meal["healthScore"] > maxHealthiness :
+            good = False
+        if good:
+            goodData.append(meal)
+    dataReturn={}
+    print(path)
+    print(len(goodData))
+    print(len(d["meals"]))
+    if (len(goodData)) == 1 :
+        dataReturn["id"] = goodData[0]["id"]
+        dataReturn["title"] = goodData[0]["title"]
+        dataReturn["image"] = goodData[0]["image"]
+        dataReturn["ingredients"] = goodData[0]["ingredients"]
+        dataReturn["kcal"] = goodData[0]["nutrition"][0]["amount"]
+        return dataReturn
+    while True :
+        index = random.randint(0,len(goodData)-1)
+        if (goodData[index]["id"] != idMeal):
+            dataReturn["id"] = goodData[index]["id"]
+            dataReturn["title"] = goodData[index]["title"]
+            dataReturn["image"] = goodData[index]["image"]
+            dataReturn["ingredients"] = goodData[index]["ingredients"]
+            dataReturn["kcal"] = goodData[index]["nutrition"][0]["amount"]
+            return dataReturn
+        
+    return null
+        
 
 
-# astea is pentru debughing, nu au traba cu programul mare
-with open (f"{abspath}/RequestType/request.json","r") as file:
-    inp = json.load(file)
 
-diet = getDiet(inp)
+def testFunction():
+    # astea is pentru debughing, nu au traba cu programul mare
+    with open (f"{abspath}/RequestType/request.json","r") as file:
+        inp = json.load(file)
 
-with open (f"{abspath}/RequestType/requestResponse.json","w") as file:
-    json.dump(diet,file, indent=2)
+    diet = getDiet(inp)
+
+    with open (f"{abspath}/RequestType/requestResponse.json","w") as file:
+        json.dump(diet,file, indent=2)
+
+print(testFunction())
+
