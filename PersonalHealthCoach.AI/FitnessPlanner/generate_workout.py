3a258e6045c2e16969e25fd895db4b11d9c79710
Victor Condurat <conduratvictor@yahoo.com>
2023-05-02 09:46:52 +0300

    Added tests

--- PersonalHealthCoach.AI/FitnessPlanner/generate_workout.py
+++ PersonalHealthCoach.AI/FitnessPlanner/generate_workout.py
@@ -1,19 +1,58 @@
 import json
 import random
+import copy
 
 def generate_workouts(user_data, exercise_database, main_muscle_groups, exercise_types):
-    goal = user_data["goal"]
-    workouts_per_week = user_data["workouts_per_week"]
-    equipment_available = user_data["equipment_available"]
-    exercises_for_goal = exercise_types[goal]
+    try:
+        goal = user_data["goal"]
+    except:
+        return {
+            "status": 400,
+            "message": "Must select a goal"
+        }
 
+    try:
+        workouts_per_week = user_data["workouts_per_week"]
+    except:
+         return {
+            "status": 400,
+            "message": "Must select a number of workouts per week"
+        }
+
+    try:
+        equipment_available = user_data["equipment_available"]
+    except:
+        equipment_available={"None":True,"Body Only":True}
+    try:
+        exercises_for_goal = exercise_types[goal]
+    except:
+        return {
+            "status": 400,
+            "message": "Invalid goal. Please choose from the available goals."
+        }
+
+    if workouts_per_week <= 0 or workouts_per_week>7:
+        return {
+            "status": 400,
+            "message": "The number of workouts per week must be greater than 0 and less than 7"
+        }
+ 
     # cazul in care useruul nu a selectat si nu avem suficiente exercitii sa-i dam
     equipment_available["None"] = True
     equipment_available["Body Only"] = True
 
-    # eliminam exercitiile pe care useru-ul nu le poate face
-    available_exercises = [e for e in exercise_database if equipment_available[e["equipment"]]]
+    # eliminam exercitiile pe care useruul nu le poate face
+    available_exercises = []
+    for exercise in exercise_database:
+        if exercise["equipment"] in equipment_available:
+            if equipment_available[exercise["equipment"]]:
+                available_exercises.append(exercise)
 
+    if not available_exercises:
+        return {
+            "status": 400,
+            "message": "No exercises available for the selected equipment. Please update the equipment list."
+        }
     # organizare pe tipri
     organized_exercises = {}
     for exercise in available_exercises:
@@ -32,6 +71,12 @@ def generate_workouts(user_data, exercise_database, main_muscle_groups, exercise
         "Increase endurance": {"Cardio": 5, "Strength": 1, "Plyometrics": 2, "Powerlifting": 0, "Olympic Weightlifting": 0, "Stretching": 0},
         "Overall health": {"Cardio": 2, "Strength": 3, "Plyometrics": 2, "Powerlifting": 0, "Olympic Weightlifting": 0, "Stretching": 1}
     }
+    for exercise_type, num in exercises_for_goal[goal].items():
+        if len(organized_exercises.get(exercise_type, [])) < num:
+            return {
+                "status": 400,
+                "message": "Not enough exercises available for the selected goal. Please update the exercise database."
+            }
     exercise_type_info = {
     "Cardio": {"rep_range": "10-15", "sets": 3, "rest_time": "1-2 minutes"},
     "Strength": {"rep_range": "8-12", "sets": 3, "rest_time": "1-2 minutes"},
@@ -65,7 +110,11 @@ def generate_workouts(user_data, exercise_database, main_muscle_groups, exercise
             exercise_info.update(exercise_type_info[exercise["type"]])
             workouts[workout_key].append(exercise_info)
 
-    return workouts
+    
+    return {
+        "status": 200,
+        "workouts": workouts
+    }
 
 def main():
     """"""
