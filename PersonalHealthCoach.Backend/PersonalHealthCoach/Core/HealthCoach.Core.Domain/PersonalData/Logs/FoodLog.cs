2d8e2f59b66e97a76479a5a6eb7cf74d925d3ed7
Stirbu.Larisa <eusuntlarisa@gmail.com>
2023-05-07 20:27:30 +0300

    Add Food Log

---
+++ PersonalHealthCoach.Backend/PersonalHealthCoach/Core/HealthCoach.Core.Domain/PersonalData/Logs/FoodLog.cs
@@ -0,0 +1,52 @@
+﻿using HealthCoach.Shared.Core;
+using CSharpFunctionalExtensions;
+
+namespace HealthCoach.Core.Domain;
+
+public sealed class FoodLog : AggregateRoot
+{
+    public FoodLog() { }
+
+    public FoodLog(Guid userId) : this()
+    {
+        Id = userId;
+        ConsumedFoods = new List<ConsumedFood>();
+        UpdatedAt = TimeProvider.Instance().UtcNow;
+    }
+
+    public static Result<FoodLog> Instance(Guid userId) => Result.Success(new FoodLog(userId));
+
+    public List<ConsumedFood> ConsumedFoods { get; private set; }
+
+    public DateTime UpdatedAt { get; private set; }
+
+    public void AddFoods(IReadOnlyCollection<ConsumedFood> foods)
+    {
+        UpdatedAt = TimeProvider.Instance().UtcNow;
+        ConsumedFoods.AddRange(foods);
+    }
+}
+
+public sealed class ConsumedFood : AggregateRoot
+{
+    public ConsumedFood() { }
+
+    public ConsumedFood(string title) : this()
+    {
+        Title = title;
+        ConsumedAt = TimeProvider.Instance().UtcNow;
+        IsNew = false;
+    }
+
+    public static ConsumedFood Create(string title) => new(title);
+
+    public string Title { get; private set; }
+
+    public DateTime ConsumedAt { get; private set; }
+    public bool IsNew { get; private set; }
+
+    public void ResetIsNew()
+    {
+        IsNew = false;
+    }
+}
