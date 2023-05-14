15be2c4108efdf549d9715b2ebe4f441ec75842f
George Cojocaru <geocaru@gmail.com>
2023-05-14 21:45:14 +0300

    fetchPersonalData

--- UserEvolution/charts/src/main.jsx
+++ UserEvolution/charts/src/main.jsx
@@ -1,3 +1,16 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <div className="app-container">
+//       <App />
+//     </div>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
