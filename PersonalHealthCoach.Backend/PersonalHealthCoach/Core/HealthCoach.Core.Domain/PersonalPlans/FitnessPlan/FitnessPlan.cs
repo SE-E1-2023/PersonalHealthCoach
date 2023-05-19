105d218835b3b26b79255db5a18ec3e83643c9a8
Tudor Tescu <tudor.tescu@hotmail.com>
2023-05-19 13:04:14 +0300

    remake fitness plan

--- PersonalHealthCoach.Backend/PersonalHealthCoach/Core/HealthCoach.Core.Domain/PersonalPlans/FitnessPlan/FitnessPlan.cs
+++ PersonalHealthCoach.Backend/PersonalHealthCoach/Core/HealthCoach.Core.Domain/PersonalPlans/FitnessPlan/FitnessPlan.cs
@@ -1,5 +1,6 @@
 ﻿using HealthCoach.Shared.Core;
 using CSharpFunctionalExtensions;
+using System.Text.Json.Serialization;
 
 namespace HealthCoach.Core.Domain;
 
@@ -40,4 +41,45 @@ public sealed class Workout : AggregateRoot
     }
 
     public IReadOnlyCollection<Exercise> Exercises { get; set; }
+}
+
+public sealed class Exercise : AggregateRoot
+{
+    public Exercise() { }
+
+    private Exercise(string name, string repRange, string restTime, int sets, string type, List<string> images, List<string> instructions)
+    {
+        Name = name;
+        RepRange = repRange;
+        RestTime = restTime;
+        Sets = sets;
+        Type = type;
+        Images = images;
+        Instructions = instructions;
+    }
+
+    public static Exercise Create(string name, string repRange, string restTime, int sets, string type,
+        List<string> images, List<string> instructions)
+        => new(name, repRange, restTime, sets, type, images, instructions);
+
+    [JsonPropertyName("exercise")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("rep_range")]
+    public string? RepRange { get; set; }
+
+    [JsonPropertyName("rest_time")]
+    public string? RestTime { get; set; }
+
+    [JsonPropertyName("sets")]
+    public int? Sets { get; set; }
+
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("images")]
+    public List<string> Images { get; set; }
+
+    [JsonPropertyName("instructions")]
+    public List<string> Instructions { get; set; }
 }
