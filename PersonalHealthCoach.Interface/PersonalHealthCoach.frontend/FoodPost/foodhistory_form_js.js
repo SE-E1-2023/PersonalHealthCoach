73a955043dd9d2e2314ac4ad98e33721eed5de5d
Tudor Tescu <tudor.tescu@hotmail.com>
2023-05-20 12:49:16 +0300

    fix food history

--- PersonalHealthCoach.Interface/PersonalHealthCoach.frontend/FoodPost/foodhistory_form_js.js
+++ PersonalHealthCoach.Interface/PersonalHealthCoach.frontend/FoodPost/foodhistory_form_js.js
@@ -1,26 +1,28 @@
+let foodCount = 1;
+
 async function FoodForm() {
     const userId = getCookie("userId");
-    const url = "http://localhost:7071/v1/users/" + userId + "/food-history";
+    const url = "http://localhost:7071/api/v1/users/" + userId + "/food-history";
 
     document.getElementById('foodForm').addEventListener('submit', function(event) {
         event.preventDefault();
 
         var form = document.getElementById('foodForm');
-        var foodName = form.elements['foodName'].value;
-        var foodType = form.elements['foodType'].value;
-        var calories = form.elements['calories'].value;
-        var quantity = form.elements['quantity'].value;
+        var data = { Foods: [] };
 
-        var data = {
-            Foods: [
-                {
-                    Title: foodName,
-                    Meal: foodType,
-                    Calories: parseInt(calories),
-                    Quantity: parseInt(quantity)
-                }
-            ]
-        };
+        for(let i = 0; i < foodCount; i++){
+            var foodName = form.elements['foodName' + i].value;
+            var foodType = form.elements['foodType' + i].value;
+            var calories = form.elements['calories' + i].value;
+            var quantity = form.elements['quantity' + i].value;
+
+            data.Foods.push({
+                Title: foodName,
+                Meal: foodType,
+                Calories: parseInt(calories),
+                Quantity: parseInt(quantity)
+            });
+        }
 
         fetch(url, {
             method: 'POST',
@@ -34,20 +36,61 @@ async function FoodForm() {
                 console.log('Form submitted successfully');
             } else {
                 console.error('Error submitting the form');
-              
             }
         })
         .catch(function(error) {
             console.error('Error submitting the form:', error);
-        
         });
     });
 
+    document.querySelector('.add-food').addEventListener('click', function(event) {
+        event.preventDefault();
+        addFood();
+    });
 
-
-
+    document.querySelector('.remove-food').addEventListener('click', function(event) {
+        event.preventDefault();
+        removeFood();
+    });
 }
 
+function addFood() {
+    const form = document.getElementById('foodForm');
+    const div = document.createElement('div');
+    div.classList.add('form-group', 'food-group');
+    div.innerHTML = `
+        <label for="foodName${foodCount}">Food Name:</label>
+        <input type="text" class="form-control" id="foodName${foodCount}" name="foodName${foodCount}" required>
+
+        <label for="foodType${foodCount}">Food Type:</label>
+        <select class="form-control" id="foodType${foodCount}" name="foodType${foodCount}" required>
+            <option value="">Select Type</option>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Drink">Drink</option>
+            <option value="Main Course">Main Course</option>
+            <option value="Side Dish">Side Dish</option>
+            <option value="Snack">Snack</option>
+            <option value="Soup">Soup</option>
+        </select>
+
+        <label for="calories${foodCount}">Calories:</label>
+        <input type="number" class="form-control" id="calories${foodCount}" name="calories${foodCount}" required>
+
+        <label for="quantity${foodCount}">Quantity:</label>
+        <input type="number" class="form-control" id="quantity${foodCount}" name="quantity${foodCount}" required>
+    `;
+    form.insertBefore(div, form.querySelector('.add-food'));
+    foodCount++;
+}
+
+function removeFood() {
+    if(foodCount > 1){
+        const form = document.getElementById('foodForm');
+        const lastFoodGroup = form.querySelectorAll('.food-group')[foodCount-1];
+        lastFoodGroup.remove();
+        foodCount--;
+    }
+}
 
 function getCookie(name) {
     const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
